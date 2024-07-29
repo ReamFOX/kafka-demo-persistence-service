f16ff3d212d46466560db4c342b4e76a18efc6f7
reamfox <sashafarion@gmail.com>
2024-07-29 10:54:48 +0300

    innit commit

---
+++ src/main/java/ua/farion/kafkademopersistenceservice/KafkaDemoPersistenceServiceApplication.java
@@ -0,0 +1,13 @@
+package ua.farion.kafkademopersistenceservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaDemoPersistenceServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaDemoPersistenceServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ua/farion/kafkademopersistenceservice/KafkaDemoPersistenceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.farion.kafkademopersistenceservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaDemoPersistenceServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
